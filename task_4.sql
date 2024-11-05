9cdd631039c5415ac656bcb4b4dc038e176ac39e
ma-kath <makathosn@gmail.com>
2024-11-05 14:45:38 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -9,7 +9,7 @@ SELECT
     COLUMN_DEFAULT,
     EXTRA
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
     AND TABLE_NAME = 'Books';
